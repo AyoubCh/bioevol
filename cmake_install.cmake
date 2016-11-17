decb5006535c93d1eaf7b7fd962921852c5e18fc
ayoubCh <aychioukh@gmail.com>
2016-11-17 10:17:34 +0100

    .gitignore

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/achioukh/PDC/pdcbioevol
+# Install script for directory: /home/achioukh/PDC/bioevolGitHub
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/home/achioukh/PDC/pdcbioevol/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/home/achioukh/PDC/bioevolGitHub/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/achioukh/PDC/pdcbioevol/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/home/achioukh/PDC/bioevolGitHub/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
